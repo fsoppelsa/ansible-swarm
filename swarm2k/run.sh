f96d1fec3c4df69310209f4a3435d94ba0f75c65
Fabrizio Soppelsa <fsoppelsa@mirantis.com>
2016-07-22 16:38:46 +0300

    Run script

--- swarm2k/run.sh
+++ swarm2k/run.sh
@@ -1,9 +1,7 @@
 #!/bin/bash
 SWARM2KID="swarm2k"
 
-#for machine in `docker-machine ls | grep $SWARM2KID | awk '{print $1;}'`;
-#do
-$machine="swarm2k0"
-MACHINE_NAME=$machine
-ansible-playbook -M library --extra-vars '{"machine_name": swarm2k0}' swarm2k/join_swarm2k.yml
-#done
+for machine in `docker-machine ls | grep $SWARM2KID | awk '{print $1;}'`;
+do
+    ansible-playbook -M library --extra-vars "{machine_name: $machine}" swarm2k/join_swarm2k.yml
+done
